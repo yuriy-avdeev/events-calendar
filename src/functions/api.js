b35c89a3f14f6403cae3fb0bd6eae41c3a16e113
Yury Avdeev <avdeiev@gmail.com>
2022-04-23 18:19:04 +0300

    linter

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,6 +1,5 @@
 const apiURL = 'https://run.mocky.io/v3/49b8fbae-13e6-4aac-a8d1-644e3881cc62'
 
-
 export const getEventsList = async () => {
   const res = await fetch(apiURL, {
     method: 'GET',
